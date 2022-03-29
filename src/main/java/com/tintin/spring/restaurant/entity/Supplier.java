8520c53cfa9cc7317b5c93ced77cc6f875dc875e
Anton Lugovoy <tintin200227@gmail.com>
2022-03-29 11:27:48 +0300

    Initial commit

---
+++ src/main/java/com/tintin/spring/restaurant/entity/Supplier.java
@@ -0,0 +1,39 @@
+package com.tintin.spring.restaurant.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "goods")
+public class Supplier {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Supplier() {
+    }
+
+    public Supplier(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
